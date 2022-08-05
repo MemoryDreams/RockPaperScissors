2a4bf9b0f5d2139580d88848bf8d905a5a8fd63d
MemoryDreams <VincentPresscod@gmail.com>
2022-08-05 18:19:01 +0300

    Nailed the reset button

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function getComputerChoice() {
     let choice;
     let i = Math.random();
+    document.getElementById("compchoice").style.animation = "rotation 2s infinite linear";
+    let l = Math.random();
+    switch (true) {
+        case (l < 0.25):
+            setTimeout(2000);
+            break;
+        case ((l > 0.25) && (l < 0.5)):
+            setTimeout(3500);
+            break;
+        case ((l > 0.5) && (l < 0.75)):
+            setTimeout(5500);
+            break;
+        case (l > 0.75):
+            setTimeout(8000);
+            break;
+    }
     switch (true) {
         case (i < 0.33):
             choice = "rock";
@@ -20,11 +36,41 @@ function getComputerChoice() {
     }
     return choice;
 }
-function getPlayerChoice() {
-    let choice;
+
+function getPlayerChoice(choice) {
+    switch (choice) {
+        case 'rock':
+            document.getElementById("rock").disabled = true;
+            document.getElementById("paper").disabled = true;
+            document.getElementById("scissors").disabled = true;
+            document.getElementById("humanchoice").src = "pics/rock.png";
+            document.getElementById("humanchoice").style.animation = "nill";
+            break;
+        case 'paper':
+            document.getElementById("rock").disabled = true;
+            document.getElementById("paper").disabled = true;
+            document.getElementById("scissors").disabled = true;
+            document.getElementById("humanchoice").src = "pics/paper.png";
+            document.getElementById("humanchoice").style.animation = "nill";
+            break;
+        case 'scissors':
+            document.getElementById("rock").disabled = true;
+            document.getElementById("paper").disabled = true;
+            document.getElementById("scissors").disabled = true;
+            document.getElementById("humanchoice").src = "pics/scissors.png";
+            document.getElementById("humanchoice").style.animation = "nill";
+            break;
+    }
 };
 
-let result;
+function reset() {
+    document.getElementById("rock").disabled = false;
+    document.getElementById("paper").disabled = false;
+    document.getElementById("scissors").disabled = false;
+    document.getElementById("humanchoice").src = "pics/favicon.png";
+    document.getElementById("humanchoice").style.animation = "rotation 2s infinite linear";
+    document.getElementById("compchoice").src = "pics/favicon.png";
+}
 
 function theGame(compChoice, playerChoice) {
 
@@ -60,5 +106,3 @@ function theGame(compChoice, playerChoice) {
     }
     return result;
 }
-
-let pchoice;
