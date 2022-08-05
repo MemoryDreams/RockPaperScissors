0ecc9c1d657ecb0c7ca1f6f06089dcceac0e09d5
MemoryDreams <VincentPresscod@gmail.com>
2022-08-05 16:03:49 +0300

    Made a function that compares player's choice and computer's choice and decides who had won

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-alert("Watch out!")
+function getComputerChoice() {
+    
+};
+
+function getPlayerChoice() {
+
+};
+
+let result;
+
+function theGame(compChoice, playerChoice) {
+
+    let result;
+
+    if (playerChoice === compChoice) {
+        result = "draw";
+    } else 
+    {
+        switch (playerChoice) {
+            case "rock":
+                if (compChoice === "scissors") {
+                    result = "win";
+                } else {
+                    result = "lose";
+                }
+                break;
+            case "scissors":
+                if (compChoice === "paper") {
+                    result = "win";
+                } else {
+                    result = "lose";
+                }
+                break;
+            case "paper":
+                if (compChoice === "rock") {
+                    result = "win";
+                } else {
+                    result = "lose";
+                }
+                break;
+        }
+    }
+    return result;
+}
+
